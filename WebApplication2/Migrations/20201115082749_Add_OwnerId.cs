8ef8ee07e007a5e6b82a1d8abce5ffcacf22d876
Thabiso Mokoena <chickott@gmail.com>
2020-11-11 04:36:13 +0200

    Added OwnerId to the provide Data

---
+++ WebApplication2/Migrations/20201115082749_Add_OwnerId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication2.Migrations
+{
+    public partial class Add_OwnerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "CMPG323project2dataset",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "CMPG323project2dataset");
+        }
+    }
+}
